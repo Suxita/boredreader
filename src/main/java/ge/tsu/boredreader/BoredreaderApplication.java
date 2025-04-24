b445d4d79f047b332c155cde0ec6b5e3fa0c3f16
mishosukhishvili <mishosukhishvili@gmail.com>
2025-04-24 04:24:39 -0700

    initial

---
+++ src/main/java/ge/tsu/boredreader/BoredreaderApplication.java
@@ -0,0 +1,13 @@
+package ge.tsu.boredreader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoredreaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoredreaderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ge/tsu/boredreader/BoredreaderApplicationTests.java
@@ -0,0 +1,13 @@
+package ge.tsu.boredreader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoredreaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
